a59cc940125f131019757cc16881be2a5c0af6b0
Young Maryclaret <mary@smartosc.com>
2018-03-02 15:00:05 +0700

    Add custom html file for displaying custom title

--- Coupon/view/frontend/requirejs-config.js
+++ Coupon/view/frontend/requirejs-config.js
@@ -6,7 +6,10 @@
 var config = {
     map: {
         '*': {
-            discountCodeCoupon: 'Claret_Coupon/js/discount-codes'
+            discountCodeCoupon: 'Claret_Coupon/js/discount-codes',
+            'Magento_SalesRule/js/view/cart/totals/discount':'Claret_Coupon/js/view/cart/totals/discount',
+            'Magento_SalesRule/js/view/summary/discount':'Claret_Coupon/js/view/summary/discount',
+            'Magento_SalesRule/template/cart/totals/discount.html':'Claret_Coupon/template/cart/totals/discount.html'
         }
     }
 };
